1594309527c102e9182aaa35ed4cab46fec70248
Jo0oelsayed74 <youssef.tawfik@ejust.edu.eg>
2024-04-28 22:48:02 +0300

    LEDMATRIX

---
+++ MiniProject2/Debug/src/MSTK_Programe.d
@@ -0,0 +1,13 @@
+src/MSTK_Programe.o: ../src/MSTK_Programe.c ../src/../include/BIT_MATH.h \
+ ../src/../include/STD_Types.h ../src/../include/MSTK_Private.h \
+ ../src/../include/MSTK_Config.h ../src/../include/MSTK_Interface.h
+
+../src/../include/BIT_MATH.h:
+
+../src/../include/STD_Types.h:
+
+../src/../include/MSTK_Private.h:
+
+../src/../include/MSTK_Config.h:
+
+../src/../include/MSTK_Interface.h:
---
+++ MiniProject2/Debug/src/main.d
@@ -0,0 +1,19 @@
+src/main.o: ../src/main.c ../src/../include/BIT_MATH.h \
+ ../src/../include/STD_Types.h ../src/../include/RCC/MRCC_Interface.h \
+ ../src/../include/GPIO/MGPIO_Interface.h \
+ ../src/../include/STK/MSTK_Interface.h ../include/../include/STD_Types.h \
+ ../src/../include/LEDMATRIX/HLEDMATRIX_Interface.h
+
+../src/../include/BIT_MATH.h:
+
+../src/../include/STD_Types.h:
+
+../src/../include/RCC/MRCC_Interface.h:
+
+../src/../include/GPIO/MGPIO_Interface.h:
+
+../src/../include/STK/MSTK_Interface.h:
+
+../include/../include/STD_Types.h:
+
+../src/../include/LEDMATRIX/HLEDMATRIX_Interface.h:
